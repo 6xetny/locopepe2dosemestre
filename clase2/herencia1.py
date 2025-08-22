c85f8947fff0df44663963de436da420d25a0658
6xetny <b4sti4n321@gmail.com>
2025-08-22 12:21:51 -0400

    D4SHI3

--- clase2/herencia1.py
+++ clase2/herencia1.py
@@ -2,11 +2,24 @@ class Vehiculo:
     def __init__(self, marca, modelo):
         self.marca = marca
         self.modelo = modelo
+        
 
     def mostrar_info(self):
-        print(f"{self.marca} {self.modelo}")
+        print(f"{self.marca} {self.modelo} ")
+
+#Clase hija
+class Auto(Vehiculo):
+    def __init__(self, marca, modelo, anio):
+        super().__init__(marca, modelo) #llamar al constructor del padre
+        self.anio = anio
+
+    def mostrar_info(self): #sobrecarga
+        print(f"{self.marca} {self.modelo} {self.anio}")
 
 #programa principal 
 vehiculo = Vehiculo("Suzuki", "Vitara")
 vehiculo.mostrar_info()
+
+auto = Auto("BMW","M3", 2010)
+auto.mostrar_info()
         
