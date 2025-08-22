edb2ba98a1cd37deed7a291aee7ff271fa69c4cf
6xetny <b4sti4n321@gmail.com>
2025-08-22 12:40:33 -0400

    hola

--- clase2/herencia1.py
+++ clase2/herencia1.py
@@ -7,6 +7,9 @@ class Vehiculo:
     def mostrar_info(self):
         print(f"{self.marca} {self.modelo} ")
 
+    def encender(self):
+        print(f"{self.marca} {self.modelo} esta encendido.")
+
 #Clase hija
 class Auto(Vehiculo):
     def __init__(self, marca, modelo, anio):
@@ -16,10 +19,22 @@ class Auto(Vehiculo):
     def mostrar_info(self): #sobrecarga
         print(f"{self.marca} {self.modelo} {self.anio}")
 
+    def encender(self):
+        print(f"{self.marca} {self.modelo} ruge al encenderse.")
+
+class Moto(Vehiculo):
+    def encender(self):
+        print(f"{self.marca} {self.modelo} arranca con rugido fuerte.")
+
 #programa principal 
 vehiculo = Vehiculo("Suzuki", "Vitara")
 vehiculo.mostrar_info()
+vehiculo.encender()
 
 auto = Auto("BMW","M3", 2010)
 auto.mostrar_info()
+auto.encender()
         
+moto = Moto("Yamaha", "R6")
+moto.mostrar_info()
+moto.encender()
