d0424c1342882447a229d0e0fb2f2252ff2c257b
6xetny <b4sti4n321@gmail.com>
2025-08-25 14:50:03 -0400

    sss

--- base_de_datos_estructuradas/clase2508.sql
+++ base_de_datos_estructuradas/clase2508.sql
@@ -4,12 +4,12 @@ USE empresa;
 
 CREATE TABLE personal
 (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE,
     salario INT,
-    departamento VARCHAR(100),
+    departamento VARCHAR(100) NOT NULL,
     fecharegistro DATE
 );
 
@@ -23,25 +23,25 @@ CREATE TABLE productos
 );
 
 /* añadir una nueva columna */
-ALTER TABLE nombre_tabla 
-  ADD nombre_columna tipo_de_dato [restricciones];
+-- ALTER TABLE nombre_tabla 
+-- ADD nombre_columna tipo_de_dato [restricciones];
 
 /* Modificar una columna existen */
-ALTER TABLE nombre_tabla
-  ALTER COLUMN nombre_columna nuevo_tipo_de_dato [restricciones];
+-- ALTER TABLE nombre_tabla
+-- ALTER COLUMN nombre_columna nuevo_tipo_de_dato [restricciones];
 
 /* Eliminar una columna */ 
-ALTER TABLE nombre_tabla
-DROP COLUMN nombre_columna;
+-- ALTER TABLE nombre_tabla
+-- DROP COLUMN nombre_columna;
 
-ALTER TABLE personal
-  ADD telefono VARCHAR(20);
+-- ALTER TABLE personal
+-- ADD telefono VARCHAR(20);
 
-ALTER TABLE personal
-  DROP COLUMN fecharegistro;
+-- ALTER TABLE personal
+-- DROP COLUMN fecharegistro;
 
 -- Eliminar tabla "productos"
-DROP TABLE productos;
+-- DROP TABLE productos;
 
 -- EN resumen CREATE construye, ALTER modifica y DROP elimina
 
@@ -52,10 +52,31 @@ INSERT INTO personal(nombre, apellido, salario, departamento)
 -- insertar multiples registros 
 INSERT INTO personal(nombre, apellido, salario, departamento)
  VALUES ('María','García',650000,'Marketing'),
-  VALUES ('Carlos','López',680000,'IT'),
-   VALUES ('Ana','Martín',620000,'Ventas');
+   ('Carlos','López',680000,'IT'),
+    ('Ana','Martín',620000,'Ventas');
 
 -- insertar sin especificar columnas (debe incluir valores para todas)
 INSERT INTO personal
   VALUES (5,'Pedro','Sánchez','ps@gmail.com',900000,'Gerencia','2025-08-21');
 
+INSERT INTO productos
+  VALUES (1,'Cuaderno',4500,20,'2025-08-21'),
+         (2,'Lápiz',2800,35,'2024-09-11'),
+         (3,'Goma',1200,15,'2023-10-31');
+
+-- La setencia  UPDATE modifica registros existentes en una tabla.
+-- Actualizar un registro en especifico 
+UPDATE personal
+SET salario = 750000
+WHERE id = 1;
+
+-- Actualizar múltiples columnas 
+UPDATE personal
+SET salario = salario * 1.10,
+    departamento = 'Ventas Senior'
+WHERE departamento = 'Ventas' AND
+    salario > 700000;
+
+-- Actualizar todos los registros 
+UPDATE productos
+    SET precio = precio * 1.05;
