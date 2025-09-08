6d689d6331765f668bf3cc203b9e6910169418f3
6xetny <b4sti4n321@gmail.com>
2025-09-08 15:23:51 -0300

    si

--- base_de_datos_estructuradas/clase3.sql
+++ base_de_datos_estructuradas/clase3.sql
@@ -20,16 +20,33 @@ insert into departamentos(id, nombre, presupuesto) values(0,'Ventas',12000000);
 insert into departamentos(id, nombre, presupuesto) values(1,'Contabilidad',14000000);
 
 insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(0,'P1P3LG', 0, 2000000,'2024-09-11');
-insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(1,'CeciliaGames', 1, 1000000,'2025-09-12');
-insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(2,'6xet', 2, 2000000,'2024-10-13');
-insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(3,'MeLlamoCesarjsjs', 3, 500000,'2024-11-14');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(1,'CeciliaGames', 1, 1000000,'2025-09-07');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(2,'6xet', 0, 2000000,'2024-10-13');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(3,'MeLlamoCesarjsjs', 1, 500000,'2024-11-14');
 
+-- vista simple: empleados con salario alto:
 create view empleados_alto_salario AS 
     select id, nombre, salario
     from empleados
     where salario > 1000000;
 
+-- vista simple: empleados nuevos:
 create view empleados_nuevos as 
     select nombre, fecha_contrato, salario
     from empleados
     where year (fecha_contrato)= year(curdate());
+
+create view empleados_detalles as 
+    select 
+        e.id,
+        e.nombre as empleado,
+        e.salario,
+        d.nombre as departamento,
+        d.presupuesto,
+        case 
+            when e.salario > 1200000 then 'Alto'
+            when e.salario > 900000 then 'Medio'
+            else 'Bajo'
+        end as nivel_salario
+        from empleados e 
+        inner join departamentos d on e.departamento_id= d.id;
