bf9e57f8bf3802936562edd3e9c84b157622219b
6xetny <b4sti4n321@gmail.com>
2025-09-08 15:01:45 -0300

    si

--- base_de_datos_estructuradas/clase3.sql
+++ base_de_datos_estructuradas/clase3.sql
@@ -15,3 +15,21 @@ create table departamentos(
     nombre varchar (50),
     presupuesto decimal(12,2)
 );
+
+insert into departamentos(id, nombre, presupuesto) values(0,'Ventas',12000000);
+insert into departamentos(id, nombre, presupuesto) values(1,'Contabilidad',14000000);
+
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(0,'P1P3LG', 0, 2000000,'2024-09-11');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(1,'CeciliaGames', 1, 1000000,'2025-09-12');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(2,'6xet', 2, 2000000,'2024-10-13');
+insert into empleados(id, nombre, departamento_id, salario,fecha_contrato) values(3,'MeLlamoCesarjsjs', 3, 500000,'2024-11-14');
+
+create view empleados_alto_salario AS 
+    select id, nombre, salario
+    from empleados
+    where salario > 1000000;
+
+create view empleados_nuevos as 
+    select nombre, fecha_contrato, salario
+    from empleados
+    where year (fecha_contrato)= year(curdate());
