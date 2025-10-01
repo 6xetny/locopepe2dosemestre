e7230e5c8ac46996b4921a2bf178b1d003893eca
6xetny <b4sti4n321@gmail.com>
2025-10-01 11:20:47 -0300

    1x1x1x1x1

--- clase2/inventario/producto_sql.py
+++ clase2/inventario/producto_sql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 conexion = mysql.connector.connect(
-    host="localhost",
+    host="127.0.0.1",
     user="root",
     # port="3307"
     #password="",
@@ -10,14 +10,22 @@ conexion = mysql.connector.connect(
 
 cursor = conexion.cursor()
 
+nombre = input("Ingrese nombre: ")
+precio = float( input("Ingrese precio: "))
+stock = int (input("Ingrese cantidad: "))
+
 #primera consulta
-cursor.execute("SHOW TABLES")
-filas = cursor.fetchall()
-print("Tablas", filas)
+
+cursor.execute("""INSERT INTO producto (nombre, precio,stock)
+                VALUES (%s, %s, %s)""", (nombre,precio,stock))
+
+# filas = cursor.fetchall()
+# print("Tablas", filas)
 
 # print("La tabllas son: ")
 # for (tabla,) in cursor:
 #     print(tabla)
 
+print("Terminamos ")
 cursor.close()
 conexion.close()
