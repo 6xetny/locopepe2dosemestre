b758b91a2ed779a9ed2012436a9b6f42e73df9a1
6xetny <b4sti4n321@gmail.com>
2025-10-30 14:32:06 -0300

    1x1x1x11x1x

--- demo_hash.py
+++ demo_hash.py
@@ -12,8 +12,24 @@ sal = secrets.token_bytes(16)
 sal_64 = base64.b64encode(sal).decode()
 print(sal_64)
 
-#2 derivamos el hash(bytes) usamdp el algoritmo pkdbf2_hmac
+
+#2 derivamos el hash(bytes) usando el algoritmo pbkdf2_hmac
 iteraciones = 100_000
-hash_b = hashlib.pkdbf2_hmac("sha256", password.encode("utf-8"), sal, iteraciones )
+hash_b = hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), sal, iteraciones )
 hash_64 = base64.b64encode(hash_b).decode()
 print(hash_64)
+print("===== Lo que deberiamos guardar como string en DB ======")
+print("Sal_64 :", sal_64)
+print("Hash_64 : ", hash_64)
+print("=========================================================")
+
+
+# 3) Verificamos contraseña 
+password_nueva = pwinput("Bienvenido, Ingrese clave: ")
+re_hash_b = hashlib.pbkdf2_hmac("sha256", password_nueva.encode("utf-8"), sal, iteraciones  )
+print(re_hash_b)
+
+
+#comparamos los hashes
+resultado = secrets.compare_digest(hash_b,re_hash_b )
+print("Son iguales los hashes?:", resultado)
